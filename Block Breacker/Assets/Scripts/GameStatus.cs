3b8fb4d94d6835808af044b693a93d173375a686
Andrew ND <andrew.nc.dm@gmail.com>
2020-10-31 15:15:32 +0200

    Provide Game speed control

---
+++ Block Breacker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.5f, 10f)]
+    float gameSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
--- Block Breacker/Assets/Scripts/Level.cs
+++ Block Breacker/Assets/Scripts/Level.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     int breakableBlocks;
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
 
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
-    }    
+    }
+
+    public void BlockDestroed()
+    {
+        if (--breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
 }
